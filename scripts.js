cedeaeeab207526327a3cee5d6485ed2dc89182f
Norbert Schnell <norbert.schnell@hs-furtwangen.de>
2023-12-23 04:33:56 +0100

    first completet version

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 const audioFiles = [
   'eot-guitar-loop.wav',
-  'eot-drum-loop.wav',
-  'eot-reverse.wav',
+  'eot-piano-reverse.wav',
+  'eot-drum-loop.wav', // level 0
+  'eot-guitar-melody-loop.wav', // level 1
+  'eot-voice-loop.wav', // level 2
+  'eot-guitar-crunch-loop.wav', // level 3
+  'eot-tail.wav',
   'eot-hit-01.wav',
   'eot-hit-02.wav',
   'eot-hit-03.wav',
@@ -14,9 +18,31 @@ const audioFiles = [
   'eot-hit-10.wav',
 ];
 
-const numHits = 10;
+const hitMarkers = [
+  0.000000,
+  1.119250,
+  1.551396,
+  1.946792,
+  3.284271,
+  3.718583,
+  4.120479,
+  5.503333,
+  5.911708,
+  6.311438
+];
+
+const numLevels = 4;
+const pointsPerLevel = 20;
+const maxPoints = (numLevels + 1) * pointsPerLevel;
+const levelOffset = 2;
+const hitOffset = numLevels + 3;
+const badDuration = 0.05;
+let startTime = 0;
 let hitIndex = 0;
-let hitOffset = 3;
+let points = 0;
+let level = 0;
+
+const loops = new Set();
 
 /********************************************************************
  * 
@@ -45,10 +71,9 @@ startScreenDiv.addEventListener("click", () => {
 function start() {
   hideOverlay();
 
-  playSound(0, 0, true);
-  playSound(2, 0, false);
-
-  // playSound(1, 0, true);
+  startTime = audioContext.currentTime;
+  playSound(0, 0, 0, true);
+  playSound(1, 0, 0, false);
 
   listenToSpaceBar();
 
@@ -95,20 +120,16 @@ function loadAudioFiles() {
   return new Promise((resolve, reject) => {
     // load audio files into audio buffers
     for (let i = 0; i < audioFiles.length; i++) {
-      const request = new XMLHttpRequest();
-      request.responseType = 'arraybuffer';
-      request.open('GET', 'sounds/' + audioFiles[i]);
-      request.addEventListener('load', () => {
-        const ac = new AudioContext();
-        ac.decodeAudioData(request.response, (buffer) => audioBuffers[i] = buffer);
-
-        numBuffersReady++;
-        if (numBuffersReady === audioFiles.length) {
-          resolve();
-        }
-      });
-
-      request.send();
+      fetch('sounds/' + audioFiles[i])
+        .then(data => data.arrayBuffer())
+        .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+        .then(decodedAudio => {
+          audioBuffers[i] = decodedAudio;
+          numBuffersReady++;
+          if (numBuffersReady === audioFiles.length) {
+            resolve();
+          }
+        });
     }
   });
 }
@@ -128,8 +149,11 @@ function requestWebAudio() {
 }
 
 // play sound by audio buffer index
-function playSound(index, amplify = 0, loop = false) {
+function playSound(index, amplify = 0, duration = 0, loop = false, offset = 0) {
+  const time = audioContext.currentTime;
   const amp = decibelToLinar(amplify);
+  const buffer = audioBuffers[index];
+  const fadeOutDuration = 0.050;
 
   const gain = audioContext.createGain();
   gain.connect(audioContext.destination);
@@ -137,14 +161,19 @@ function playSound(index, amplify = 0, loop = false) {
 
   const source = audioContext.createBufferSource();
   source.connect(gain);
-  source.buffer = audioBuffers[index];
+  source.buffer = buffer;;
   source.loop = loop;
-  source.start(audioContext.currentTime);
-}
+  source.start(time, offset);
+ 
+  if (duration > 0 && duration < (buffer.duration - fadeOutDuration)) {
+    gain.gain.setValueAtTime(amp, time + duration);
+    gain.gain.linearRampToValueAtTime(0, time + duration + fadeOutDuration);
+    source.stop(time + duration + fadeOutDuration);
+  }
 
-function playNextHit() {
-  playSound(hitIndex + hitOffset, 6);
-  hitIndex = (hitIndex + 1) % numHits;
+  if (loop) {
+    loops.add({ source, gain, amp });
+  }
 }
 
 function decibelToLinar(val) {
@@ -157,7 +186,7 @@ function decibelToLinar(val) {
 function listenToSpaceBar() {
   document.addEventListener('keyup', event => {
     if (event.code === 'Space') {
-      playNextHit();
+      doHit();
     }
   })
 }
@@ -211,11 +240,13 @@ function listenToDeviceMotion() {
   window.addEventListener("devicemotion", onDeviceMotion);
 }
 
-const rotationRateThreshold = 240;
+const rotationRateThreshold = 200;
 let filterCoeff = null;
 let lastFilteredRot = 0;
 let lastDiffRot = null;
 let lastHitTime = -Infinity;
+
+let markerIndex = -1;
 
 function onDeviceMotion(e) {
   if (dataStreamTimeout !== null && dataStreamResolve !== null) {
@@ -242,12 +273,84 @@ function onDeviceMotion(e) {
     const peakTime = 0.001 * performance.now();
     const peakRot = currentFilteredRot;
 
-    if (peakRot >= rotationRateThreshold && (peakTime - lastHitTime) > 0.25) {
-      playNextHit();
+    const timeSinceLastHit = peakTime - lastHitTime;
+    if (peakRot >= rotationRateThreshold && timeSinceLastHit > 0.25) {
+      doHit();
       lastHitTime = peakTime;
     }
   }
 
   lastFilteredRot = currentFilteredRot;
   lastDiffRot = currentDiffRot;
+}
+
+function doHit() {
+  const time = audioContext.currentTime;
+  const loopDuration = audioBuffers[0].duration;
+  const loopTime = (time - startTime) % loopDuration;
+
+  const tolerance = audioBuffers[0].duration / 64;
+  const tolerantLoopTime = (loopTime + tolerance) % loopDuration;
+  hitIndex = getCurrentOrPreviousIndex(hitMarkers, tolerantLoopTime, hitIndex);
+
+  const diff = (hitMarkers[hitIndex] + tolerance - tolerantLoopTime);
+  const good = (diff > -tolerance && diff < tolerance);
+  const duration = good ? 0 : badDuration;
+  playSound(hitIndex + hitOffset, 6, duration);
+
+  points += good ? 1 : -1;
+  displayPoints(points);
+
+  if (hitIndex === 0 && points > pointsPerLevel * (level + 1) && level < numLevels) {
+    playSound(levelOffset + level, 0, 0, true, loopTime);
+    level++;
+  } else if (level == 2 && hitIndex === 9 && points > pointsPerLevel * (level + 1) - 1 && level < numLevels) {
+    playSound(levelOffset + level, 0, 0, true, loopTime);
+    level++;
+  } else if (level == numLevels && hitIndex === 0 && points >= maxPoints) {
+    playSound(levelOffset + numLevels, 0, 0, false, loopTime);
+
+    const fadeOutDuration = 0.050;
+
+    // kill all loops
+    for (let loop of loops) {
+      const source = loop.source;
+      const gain = loop.gain;
+      gain.gain.setValueAtTime(loop.amp, time);
+      gain.gain.linearRampToValueAtTime(0, time + fadeOutDuration);
+      source.stop(time + fadeOutDuration);
+    }
+  }
+}
+
+function getCurrentOrPreviousIndex(sortedArray, value, index = -1) {
+  var size = sortedArray.length;
+
+  if (size > 0) {
+    var firstVal = sortedArray[0];
+    var lastVal = sortedArray[size - 1];
+
+    if (value < firstVal)
+      index = -1;
+    else if (value >= lastVal)
+      index = size - 1;
+    else {
+      if (index < 0 || index >= size)
+        index = Math.floor((size - 1) * (value - firstVal) / (lastVal - firstVal));
+
+      while (sortedArray[index] > value)
+        index--;
+
+      while (sortedArray[index + 1] <= value)
+        index++;
+    }
+  }
+
+  return index;
+}
+
+const pointsDiv = document.getElementById("points");
+
+function displayPoints(points) {
+  pointsDiv.innerHTML = points;
 }
